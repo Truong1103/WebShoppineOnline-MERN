fc99153c310ae883b93c9b4c3b8eeac7a5b0ff4e
Truong1103 <gymptw1@gmail.com>
2024-08-23 21:40:25 +0700

    xoa pass mongodb

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,7 +1,7 @@
 const MyConstants = {
     DB_SERVER: 'clusternht.eit1sig.mongodb.net',
     DB_USER: 'gymptw1',
-    DB_PASS: 'anhem123',
+    DB_PASS: '?????????????',
     DB_DATABASE: 'shoppingonline',
     JWT_SECRET: 'nguyenhuutruong',
     JWT_EXPIRES: '360000000', // in milliseconds
